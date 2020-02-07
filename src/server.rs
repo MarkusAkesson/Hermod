befcdd3cda79116e84fc2b4ce5902940fdfa361c
dat14mak <dat14mak@student.lu.se>
2020-02-07 10:50:07 +0100

    Fix compile errors

--- src/server.rs
+++ src/server.rs
@@ -1,9 +1,9 @@
 pub struct Server {
-    hostname: String,
-    id_token: String,
-    public_key: Vec<u8>,
-    private_key: Vec<u8>,
-    server_key: Vec<u8>,
+    pub hostname: String,
+    pub id_token: String,
+    pub public_key: Vec<u8>,
+    pub private_key: Vec<u8>,
+    pub server_key: Vec<u8>,
 }
 
 impl Server {}
