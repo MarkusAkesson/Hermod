4dc6a76b29354598c64c0186fc467c5366969ff6
Markus Åkesson <macke.95_06@hotmail.com>
2020-02-11 16:28:53 +0100

    Add upload request

--- src/request.rs
+++ src/request.rs
@@ -1,10 +1,18 @@
 use crate::config::ClientConfig;
-#[derive(Copy, Clone, Eq, PartialEq)]
+use crate::message::{Message, MessageType};
+
+use async_std::fs::File;
+use async_std::io::{BufReader, BufWriter};
+use async_std::prelude::*;
+use serde::{Deserialize, Serialize};
+
+#[derive(Copy, Clone, Eq, PartialEq, Serialize, Deserialize)]
 pub enum RequestMethod {
     Upload = 1,
     Download,
 }
 
+#[derive(Eq, PartialEq, Serialize, Deserialize)]
 pub struct Request<'a> {
     source: &'a str,
     destination: &'a str,
@@ -18,5 +26,43 @@ impl<'a> Request<'a> {
             destination: &config.destination,
             method: config.request,
         }
+    }
+
+    pub async fn send(&self) {
+        unimplemented!();
+    }
+
+    pub async fn exec(&self) {
+        match self.method {
+            RequestMethod::Upload => self.upload().await,
+            RequestMethod::Download => self.download().await,
+        }
+    }
+
+    // read a file and send it to a task responsible for sending the msg to peer
+    async fn upload(&self) {
+        let file = File::open(self.source).await.unwrap();
+        let mut buf_reader = BufReader::new(file);
+        let (tx, rx) = async_std::sync::channel(100);
+        loop {
+            let mut buffer = Vec::with_capacity(1024);
+            let n = buf_reader
+                .by_ref()
+                .take(1024)
+                .read_to_end(&mut buffer)
+                .await
+                .unwrap();
+            if n == 0 {
+                // EOF reached
+                break;
+            }
+            let msg = Message::new(&buffer, MessageType::Payload);
+            tx.send(msg);
+        }
+    }
+
+    async fn download(&self) {
+        let file = File::create(self.source).await.unwrap();
+        let buf_reader = BufWriter::new(file);
     }
 }
