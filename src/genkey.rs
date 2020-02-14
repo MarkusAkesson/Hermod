f131d299327af484546d081e7140f9c545371410
dat14mak <dat14mak@student.lu.se>
2020-02-14 14:42:08 +0100

    Add binary hermod, move server out of hermod module and into server.rs

--- src/genkey.rs
+++ src/genkey.rs
@@ -2,6 +2,10 @@ use crate::consts::*;
 
 use snow::Builder;
 
+pub fn gen_server_keys() -> Result<(), snow::error::Error> {
+    unimplemented!()
+}
+
 pub fn gen_key() -> Result<snow::Keypair, snow::error::Error> {
     let builder = Builder::new(NOISE_PATTERN.parse()?);
     builder.generate_keypair()
