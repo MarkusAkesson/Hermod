056c3bd68a17ac881961cbbfcfeab6c7b40ebb39
dat14mak <dat14mak@student.lu.se>
2020-04-20 11:45:49 +0200

    Add sftp and scp to pcap.py

--- pcap.py
+++ pcap.py
@@ -1,6 +1,8 @@
 from scapy.all import *
 
 HERMOD_PORT = 4444
+SFTP_PORT = 22
+
 
 def hermod_len():
     scapy_cap = rdpcap('output/hermod.pcap')
@@ -14,4 +16,18 @@ def hermod_len():
     return transfer_len
 
 
+def sftp_len(file_name):
+    scapy_cap = rdpcap('output/{}.pcap'.format(file_name))
+    transfer_len = 0
+    for packet in scapy_cap:
+        if TCP not in packet.layers():
+            continue
+        if packet[TCP].dport != SFTP_PORT:
+            continue
+        transfer_len += packet.payload.len
+    return transfer_len
+
+
 print("Hermod session length: {}".format(hermod_len()))
+print("SCP session length: {}".format(sftp_len('scp')))
+print("SFTP session length: {}".format(sftp_len('sftp')))
