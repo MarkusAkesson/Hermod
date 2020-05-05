6600602b78b8b663efbda0c9f75a589706b531ad
Markus Åkesson <macke.95_06@hotmail.com>
2020-05-05 21:09:21 +0200

    Use lxc for benchmarking

--- run-bench.sh
+++ run-bench.sh
@@ -4,23 +4,18 @@ set -e
 
 if [ "$1" = "server" ]; then
     /usr/sbin/sshd
-    hermod server --no-daemon
+    hermod server
     exit
 fi
 
+HERMOD_SRC='/root/Hermod'
 SRC_DIR="/sources"
 OUT_DIR="/output"
 
 REMOTE="bench"
-HOST_NAME=bench-server
+HOST_NAME=eoan
 
 srcs=large.file,medium.file,small.file,src
-
-
-# Share keys with server
-hermod share-key --host $HOST_NAME --name $REMOTE
-ssh-keygen -t rsa -b 4096 -f ~/.ssh/id_rsa
-ssh-copy-id -i ~/.ssh/id_rsa root@$HOST_NAME
 
 # Upload files
 hyperfine --parameter-list src ${srcs[@]} "hermod upload --source $SRC_DIR/{src} --remote $REMOTE --destination $OUT_DIR" --export-json /output/hermod.json
@@ -30,3 +25,4 @@ hyperfine --parameter-list src ${srcs[@]} "scp -r $SRC_DIR/{src} root@$HOST_NAME
 hyperfine --parameter-list src ${srcs[@]} "sftp root@$HOST_NAME <<EOF
 put -r $SRC_DIR/{src} $OUT_DIR
 EOF" --export-json /output/sftp.json
+
