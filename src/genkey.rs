e8ab57099c70de2be7c53fd036ddf2c80057fdb9
Markus Åkesson <macke.95_06@hotmail.com>
2021-11-19 21:22:29 +0100

    Add tests for genkey

--- src/genkey.rs
+++ src/genkey.rs
@@ -18,3 +18,34 @@ pub fn gen_idtoken() -> String {
     rand::thread_rng().fill_bytes(&mut vec);
     base64::encode(&vec)
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::genkey::*;
+
+    #[test]
+    fn diff_gen_keys() {
+        let key1 = gen_keys().unwrap();
+        let key2 = gen_keys().unwrap();
+
+        assert_ne!(key1.private, key2.private);
+        assert_ne!(key1.public, key2.public);
+    }
+
+    #[test]
+    fn diff_server_keys() {
+        let key1 = create_server_keys().unwrap();
+        let key2 = create_server_keys().unwrap();
+
+        assert_ne!(key1.private, key2.private);
+        assert_ne!(key1.public, key2.public);
+    }
+
+    #[test]
+    fn diff_gen_idtoken() {
+        let token1 = gen_idtoken();
+        let token2 = gen_idtoken();
+
+        assert_ne!(token1, token2);
+    }
+}
